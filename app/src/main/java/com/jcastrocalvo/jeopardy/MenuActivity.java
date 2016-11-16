8c722e59960ee84f05c84ee5fcd9d602c59c9895
jcastrocalvo <joaquin.castro-calvo@marquette.edu>
2016-11-15 22:56:00 -0600

    Jeopardy Embedded System Game
    Jeopardy game created for Android and the LandTiger with the LPC1768.

---
+++ app/src/main/java/com/jcastrocalvo/jeopardy/MenuActivity.java
@@ -0,0 +1,22 @@
+package com.jcastrocalvo.jeopardy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, BoardActivity.class);
+        startActivity(intent);
+    }
+}
